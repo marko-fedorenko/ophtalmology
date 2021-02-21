d3a3cea11079a662928d9ae94124cb69a4758dfe
marko-fedorenko <marko.fedorenko@gmail.com>
2021-02-21 18:41:36 +0000

    Added Carousel

--- js/index.js
+++ js/index.js
@@ -1,4 +1,59 @@
-// Crude way to check that the screen is at least 1024x768
-if (window.screen.width >= 720) {
-  // Resolution is 1024x768 or above
+const images = [
+    'img/bloodcells.jpg',
+    'img/bloodcells-2.jpg',
+    'img/bloodcells-3.jpg'
+]
+
+let currIdx = 0
+
+function showCurrImage() {
+    const imageContainer = document.querySelector('.current')
+    imageContainer.src = images[currIdx];
 }
+
+showCurrImage();
+
+function nextImage() {
+    currIdx++;
+    if (currIdx >= images.length) currIdx = 0; 
+    showCurrImage();
+}
+
+function prevtImage() {
+    currIdx--;
+    if (currIdx < 0) currIdx = images.length - 1; 
+    showCurrImage();
+}
+
+document.querySelector('.prev').addEventListener('click', prevtImage)
+document.querySelector('.next').addEventListener('click', nextImage)
+//carousell end
+
+const imageDescription = [
+    'Red blood cells, carrying oxygen and nutrients throughout the body',
+    'White blood cells, which fight infection and help you recover',
+    'Platelets, which are responsible for the formation of clots to stop bleeding'
+]
+
+let currDescIdx = 0
+
+function showCurrDescription() {
+    const textContainer = document.querySelector('.image-description');
+    textContainer.innerText = imageDescription[currDescIdx];
+}
+showCurrDescription();
+
+function nextImageDescription() {
+    currDescIdx++;
+    if (currDescIdx >= imageDescription.length) currDescIdx = 0; 
+    showCurrDescription();
+}
+
+function prevtImageDescription() {
+    currDescIdx--;
+    if (currDescIdx < 0) currDescIdx = imageDescription.length - 1; 
+    showCurrDescription();
+}
+
+document.querySelector('.prev').addEventListener('click', prevtImageDescription)
+document.querySelector('.next').addEventListener('click', nextImageDescription)
